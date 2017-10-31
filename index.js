bad38d106857d7c3484679b7baa017281fa03ebb
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-10-30 22:18:14 -0400

    Move remaining initialization logic into 'lib/index.js'.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const parseArgs = require( 'minimist' );
 
 // Project
 const goalist = require( './lib' );
-const utils = require( './utils' );
+
 
 // --------------------------------------------------
 // DECLARE VARS
@@ -20,41 +20,18 @@ const utils = require( './utils' );
 var COMMAND = process.argv.slice( 2, 3 );
 var ARGS = parseArgs( process.argv.slice( 2 ) );
 
-// --------------------------------------------------
-// DECLARE FUNCTIONS
-// --------------------------------------------------
-function init( ARGS ) {
-	// Ensure that `gl` is invoked with at least 1x arg.
-	if ( !ARGS._[ 0 ] ) {
-		console.log( 'Whoops, `goalist` must be executed with a valid command.' );
-		return;
-	}
-
-	/// TODO: Consider building out wrapper function around `getOrCreate*()` methods.
-	let goalistDirData = utils.getOrCreateGoalistDir();
-	let logsDirData = utils.getOrCreateLogsDir();
-	let todayLogData = utils.getOrCreateTodayLog();
-
-	// Invoke `COMMAND` if it's valid, log error message otherwise.
-	 if ( COMMAND in goalist.commands ) {
-		 goalist.commands[ COMMAND ]( ARGS )
-			 .then(
-				 ( data ) => {
-					 // DO THE NEEDFUL;
-				 }
-			 )
-			 .catch(
-				 ( err ) => {
-
-				 }
-			 );
-	} else {
-		console.log( 'Whoops, `goalist` was invoked with an invalid command.' );
-		return;
-	}
-}
 
 // --------------------------------------------------
 // INITIALIZATION
 // --------------------------------------------------
-init( ARGS );
+goalist
+	.preflight( [ COMMAND, ARGS ] )
+	.then( goalist.run )
+	.then(
+		( data ) => {
+			/// TODO
+		},
+		( err ) => {
+			/// TODO
+		}
+	);
