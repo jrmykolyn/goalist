49166e2ac06554030bd6d298db4ae2c4ac66d8c2
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-01-18 09:05:27 -0500

    topic-35 Update 'Goalist' tests: ensure that 'Utils' can be configured at instantiation.

--- test/goalist.test.js
+++ test/goalist.test.js
@@ -2,9 +2,11 @@
 // IMPORT MODULES
 // --------------------------------------------------
 // Node
+const fs = require( 'fs' );
 
 // Vendor
 const test = require( 'ava' );
+const del = require( 'del' );
 
 // Project
 const pkg = require( '../package' );
@@ -15,9 +17,41 @@ const pkg = require( '../package' );
 // NOTE: Make sure that import path is relative to process dir. (eg. root).
 let importPath = `${process.cwd()}/${pkg.main}`;
 
+const setupOpts = {
+	goalistDir: `${__dirname}/temp_${parseInt( Math.random() * new Date().getTime() )}`,
+	logsDirName: 'logs',
+	logIdentifier: '1989-12-29',
+	logName: 'goalist_active.log',
+	logData: {
+		goals: {
+			'1234567890': {
+				id: '1234567890',
+				title: 'My Cool Goal.'
+			},
+		},
+	},
+};
+
 // --------------------------------------------------
 // DECLARE TESTS: SETUP AND TEARDOWN
 // --------------------------------------------------
+test.before( () => {
+	let {
+		goalistDir,
+		logsDirName,
+		logName,
+		logData,
+	} = setupOpts;
+
+	fs.mkdirSync( goalistDir );
+	fs.mkdirSync( `${goalistDir}/${logsDirName}` );
+
+	fs.writeFileSync( `${goalistDir}/${logsDirName}/${logName}`, JSON.stringify( logData ), 'utf8' );
+} );
+
+test.after.always( () => {
+	del.sync( setupOpts.goalistDir );
+} );
 
 // --------------------------------------------------
 // DECLARE TESTS
@@ -53,3 +87,17 @@ test( 'Exposes the `run()` instance method.', ( t ) => {
 
 	t.is( typeof goalist.run, 'function' );
 } );
+
+test( 'Allows `Utils` to be configured at instantiation.', ( t ) => {
+	const Goalist = require( importPath );
+
+	let goalist = new Goalist();
+
+	return goalist.run( 'list', [], { utilsOpts: { path: setupOpts.goalistDir } } )
+		.then( ( data ) => {
+			t.pass();
+		} )
+		.catch( ( err ) => {
+			t.fail();
+		} );
+} )
