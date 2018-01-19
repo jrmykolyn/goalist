f2a42fa1eb1f8a69ebef3782f86c2e1d23123a10
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-01-19 08:59:51 -0500

    topic-37 Update interfaces.

--- src/interfaces/goalist.ts
+++ src/interfaces/goalist.ts
@@ -1,11 +1,23 @@
 // --------------------------------------------------
+// IMPORT MODULES
+// --------------------------------------------------
+import { UtilsInstance } from './';
+
+// --------------------------------------------------
 // PUBLIC API
 // --------------------------------------------------
+export interface GoalistInstance {
+	utilsRef: UtilsInstance,
+};
+
+export interface GoalistOptions {
+	utilsOpts?: any,
+};
+
 export interface GoalistInput {
 	[ index: number ]: string,
-}
+};
 
 export interface GoalistArgs {
 	verbose?: boolean,
-	utilsOpts?: any,
 };
--- src/interfaces/utils.ts
+++ src/interfaces/utils.ts
@@ -5,6 +5,10 @@ export interface UtilsInstance {
 	goalistDirName: any,
 	goalistDirRoot: any,
 	goalistDirPath: any,
+	getOrCreateGoalistDir: any,
+	getOrCreateLogsDir: any,
+	getOrCreateBakDir: any,
+	getOrCreateLog: any,
 };
 
 export interface UtilsStore {
