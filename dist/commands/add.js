7c8835425fcb53ff89b0ec4a3cd4961809f2cfe1
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-04-19 23:52:16 -0400

    Update 'add': decouple validation from function body

--- dist/commands/add.js
+++ dist/commands/add.js
@@ -1,30 +1,34 @@
 "use strict";
 exports.__esModule = true;
+var utils_1 = require("./utils");
 function add(INPUT, ARGS, config) {
-    return new Promise(function (resolve, reject) {
-        var title = INPUT[0] || null;
-        if (!title) {
-            var err = 'Whoops, `add` must be invoked with a valid `title` argument.';
-            config["debugger"].log(err);
-            reject(new Error(err));
-            return;
-        }
-        var log = config.utils.getLog('active');
-        var goals = log.goals;
-        var id = config.utils.generateId();
-        var goal = {
-            id: id,
-            title: title,
-            category: ARGS.category || '',
-            description: ARGS.description || '',
-            tags: ARGS.tags ? ARGS.tags.split(',').map(function (str) { return str.trim(); }) : [],
-            complete: false,
-            active: true
-        };
-        goals[id] = goal;
-        config.utils.writeLog('active', JSON.stringify(log));
-        config["debugger"].log("Successfully created goal: " + id);
-        resolve(goal);
-    });
+    var title = INPUT[0] || null;
+    var log = config.utils.getLog('active');
+    var goals = log.goals;
+    var id = config.utils.generateId();
+    var goal = {
+        id: id,
+        title: title,
+        category: ARGS.category || '',
+        description: ARGS.description || '',
+        tags: ARGS.tags ? ARGS.tags.split(',').map(function (str) { return str.trim(); }) : [],
+        complete: false,
+        active: true
+    };
+    goals[id] = goal;
+    config.utils.writeLog('active', JSON.stringify(log));
+    config["debugger"].log("Successfully created goal: " + id);
+    return goal;
 }
-exports["default"] = add;
+var hasValidTitle = function (INPUT, ARGS, config) {
+    return new Promise(function (resolve, reject) {
+        var title = INPUT[0];
+        if (!title) {
+            reject(new Error('Whoops, `add` must be invoked with a valid `title` argument.'));
+        }
+        else {
+            resolve();
+        }
+    });
+};
+exports["default"] = utils_1["default"](add, [hasValidTitle]);
--- src/commands/add.ts
+++ src/commands/add.ts
@@ -7,6 +7,7 @@ import * as fs from 'fs';
 // Vendors
 
 // Project
+import makeCommand from './utils';
 import { Goal, GoalistArgs, GoalistConfig, GoalistInput } from '../interfaces';
 
 // --------------------------------------------------
@@ -16,41 +17,46 @@ import { Goal, GoalistArgs, GoalistConfig, GoalistInput } from '../interfaces';
 // --------------------------------------------------
 // DECLARE FUNCTIONS
 // --------------------------------------------------
-export default function add( INPUT: GoalistInput, ARGS: GoalistArgs, config: GoalistConfig ): Promise<Goal> {
+function add( INPUT: GoalistInput, ARGS: GoalistArgs, config: GoalistConfig ): Goal {
+	let title = INPUT[ 0 ] || null;
+
+	let log = config.utils.getLog( 'active' );
+	let { goals } = log;
+
+	// Create and update `goal`.
+	let id = config.utils.generateId();
+	let goal: Goal = {
+		id: id,
+		title: title,
+		category: ARGS.category || '',
+		description: ARGS.description || '',
+		tags: ARGS.tags ? ARGS.tags.split( ',' ).map((str) => str.trim()) : [],
+		complete: false,
+		active: true,
+	};
+
+	// Add `goal`.
+	goals[ id ] = goal;
+
+	// Write new data back to file system.
+	config.utils.writeLog( 'active', JSON.stringify( log ) );
+
+	config.debugger.log( `Successfully created goal: ${id}` );
+
+	return goal;
+}
+
+const hasValidTitle = ( INPUT: GoalistInput, ARGS: GoalistArgs, config: GoalistConfig ) => {
 	return new Promise( ( resolve, reject ) => {
-		let title = INPUT[ 0 ] || null;
+		const [title] = INPUT;
 
 		if ( !title ) {
-			let err = 'Whoops, `add` must be invoked with a valid `title` argument.';
-
-			config.debugger.log( err );
-			reject( new Error( err ) );
-			return;
+			reject( new Error( 'Whoops, `add` must be invoked with a valid `title` argument.' ) );
+		} else {
+			resolve();
 		}
-
-		let log = config.utils.getLog( 'active' );
-		let { goals } = log;
-
-		// Create and update `goal`.
-		let id = config.utils.generateId();
-		let goal: Goal = {
-			id: id,
-			title: title,
-			category: ARGS.category || '',
-			description: ARGS.description || '',
-			tags: ARGS.tags ? ARGS.tags.split( ',' ).map((str) => str.trim()) : [],
-			complete: false,
-			active: true,
-		};
-
-		// Add `goal`.
-		goals[ id ] = goal;
-
-		// Write new data back to file system.
-		config.utils.writeLog( 'active', JSON.stringify( log ) );
-
-		config.debugger.log( `Successfully created goal: ${id}` );
-
-		resolve( goal );
 	} );
-}
+};
+
+
+export default makeCommand( add, [ hasValidTitle ] );
