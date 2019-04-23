eab13d9a6a30842a523383025dd1ebfdc3d1d089
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-04-23 07:56:22 -0400

    Decouple 'remove' command validation and logic

--- dist/commands/remove.js
+++ dist/commands/remove.js
@@ -1,40 +1,31 @@
 "use strict";
 exports.__esModule = true;
+var utils_1 = require("./utils");
+var validators_1 = require("../validators");
 function remove(INPUT, ARGS, config) {
-    return new Promise(function (resolve, reject) {
-        var identifier = INPUT[0] || null;
-        if (!identifier) {
-            var err = 'Whoops, `remove` must be invoked with a valid `identifier` argument.';
-            config["debugger"].log(err);
-            reject(new Error(err));
-            return;
-        }
-        var log = ARGS.archive ? config.utils.getLog('archive') : config.utils.getLog('active');
-        var goals = log.goals;
-        var goal = goals[identifier] || null;
-        var userConf = null;
-        if (!goal) {
-            var err = "Whoops, failed to find a goal which matches the following identifier: " + identifier;
-            config["debugger"].log(err);
-            reject(new Error(err));
-            return;
-        }
-        if (!config.cli || ARGS.force) {
-            config["debugger"].log("Removing task: " + identifier);
-            for (var key in goals) {
-                if (goals[key] === goal) {
-                    delete goals[key];
-                }
+    var identifier = INPUT[0] || null;
+    var log = ARGS.archive ? config.utils.getLog('archive') : config.utils.getLog('active');
+    var goals = log.goals;
+    var goal = goals[identifier] || null;
+    var userConf = null;
+    if (!goal) {
+        var err = "Whoops, failed to find a goal which matches the following identifier: " + identifier;
+        config["debugger"].log(err);
+        throw new Error(err);
+    }
+    if (!config.cli || ARGS.force) {
+        config["debugger"].log("Removing task: " + identifier);
+        for (var key in goals) {
+            if (goals[key] === goal) {
+                delete goals[key];
             }
-            config.utils.writeLog(ARGS.archive ? 'archive' : 'active', JSON.stringify(log));
-            resolve(log);
-            return;
         }
-        else {
-            config["debugger"].log('This is a destructive action and can only be executed if the `--force` flag is provided. Aborting.');
-            reject(log);
-            return;
-        }
-    });
+        config.utils.writeLog(ARGS.archive ? 'archive' : 'active', JSON.stringify(log));
+        return log;
+    }
+    else {
+        config["debugger"].log('This is a destructive action and can only be executed if the `--force` flag is provided. Aborting.');
+        return;
+    }
 }
-exports["default"] = remove;
+exports["default"] = utils_1["default"](remove, [validators_1.hasValidInput({ msg: 'Whoops, `remove` must be invoked with a valid `identifier` argument.' })]);
