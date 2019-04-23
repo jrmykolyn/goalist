a6e7485bb5e40899eb7078e195a70a299b9244b5
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-04-23 08:02:05 -0400

    Decouple 'update' command validation and logic

--- dist/commands/update.js
+++ dist/commands/update.js
@@ -1,32 +1,26 @@
 "use strict";
 exports.__esModule = true;
+var utils_1 = require("./utils");
+var validators_1 = require("../validators");
 var keyBlacklist = ['_'];
 function update(INPUT, ARGS, config) {
-    return new Promise(function (resolve, reject) {
-        var identifier = INPUT[0] || null;
-        if (!identifier) {
-            var err = 'Whoops, `update` must be invoked with a valid `identifier` argument.';
-            config["debugger"].log(err);
-            reject(new Error(err));
-            return;
+    var identifier = INPUT[0] || null;
+    var log = config.utils.getLog('active');
+    var goals = log.goals;
+    var goal = goals[identifier] || null;
+    if (!goal) {
+        var err = "Whoops, failed to find a goal which matches the following identifier: " + identifier;
+        config["debugger"].log(err);
+        throw new Error(err);
+        return;
+    }
+    for (var key in ARGS) {
+        if (goal[key] !== 'undefined' && keyBlacklist.indexOf(key) === -1) {
+            goal[key] = ARGS[key];
         }
-        var log = config.utils.getLog('active');
-        var goals = log.goals;
-        var goal = goals[identifier] || null;
-        if (!goal) {
-            var err = "Whoops, failed to find a goal which matches the following identifier: " + identifier;
-            config["debugger"].log(err);
-            reject(new Error(err));
-            return;
-        }
-        for (var key in ARGS) {
-            if (goal[key] !== 'undefined' && keyBlacklist.indexOf(key) === -1) {
-                goal[key] = ARGS[key];
-            }
-        }
-        config.utils.writeLog('active', JSON.stringify(log));
-        config["debugger"].log("Successfully updated the following properties: " + Object.keys(ARGS).filter(function (key) { return key !== '_'; }).join('; '));
-        resolve(goal);
-    });
+    }
+    config.utils.writeLog('active', JSON.stringify(log));
+    config["debugger"].log("Successfully updated the following properties: " + Object.keys(ARGS).filter(function (key) { return key !== '_'; }).join('; '));
+    return goal;
 }
-exports["default"] = update;
+exports["default"] = utils_1["default"](update, [validators_1.hasValidInput({ msg: 'Whoops, `update` must be invoked with a valid `identifier` argument.' })]);
