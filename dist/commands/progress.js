d5358f6513fb334d11e7419db33e21bbeb9a287f
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-04-29 07:50:47 -0400

    topic-59 Refactor 'progress'

--- dist/commands/progress.js
+++ dist/commands/progress.js
@@ -1,18 +1,17 @@
 "use strict";
 exports.__esModule = true;
+var utils_1 = require("./utils");
 function progress(INPUT, ARGS, config) {
-    return new Promise(function (resolve, reject) {
-        var log = ARGS.archive ? config.utils.getLog('archive') : config.utils.getLog('active');
-        var goals = log.goals;
-        var total = Object.keys(goals).length;
-        var complete = config.utils.getComplete(goals).length;
-        var incomplete = total - complete;
-        resolve({
-            type: ARGS.archive ? 'archive' : 'active',
-            total: total,
-            complete: complete,
-            incomplete: incomplete
-        });
-    });
+    var log = ARGS.archive ? config.utils.getLog('archive') : config.utils.getLog('active');
+    var goals = log.goals;
+    var total = Object.keys(goals).length;
+    var complete = config.utils.getComplete(goals).length;
+    var incomplete = total - complete;
+    return {
+        type: ARGS.archive ? 'archive' : 'active',
+        total: total,
+        complete: complete,
+        incomplete: incomplete
+    };
 }
-exports["default"] = progress;
+exports["default"] = utils_1["default"](progress, [function () { return Promise.resolve(); }]);
--- src/commands/progress.ts
+++ src/commands/progress.ts
@@ -7,6 +7,7 @@
 import * as barHorizontal from 'bar-horizontal';
 
 // Project
+import makeCommand from './utils';
 import { Goal, GoalistArgs, GoalistConfig, GoalistInput, GoalistProgress } from '../interfaces';
 
 // --------------------------------------------------
@@ -16,20 +17,20 @@ import { Goal, GoalistArgs, GoalistConfig, GoalistInput, GoalistProgress } from 
 // --------------------------------------------------
 // DECLARE FUNCTIONS
 // --------------------------------------------------
-export default function progress( INPUT: GoalistInput, ARGS: GoalistArgs, config: GoalistConfig ): Promise<GoalistProgress> {
-	return new Promise( ( resolve, reject ) => {
-		let log = ARGS.archive ? config.utils.getLog( 'archive' ) : config.utils.getLog( 'active' );
-		let { goals } = log;
+function progress( INPUT: GoalistInput, ARGS: GoalistArgs, config: GoalistConfig ): GoalistProgress {
+	let log = ARGS.archive ? config.utils.getLog( 'archive' ) : config.utils.getLog( 'active' );
+	let { goals } = log;
 
-		let total = Object.keys( goals ).length;
-		let complete = config.utils.getComplete( goals ).length;
-		let incomplete = total - complete;
+	let total = Object.keys( goals ).length;
+	let complete = config.utils.getComplete( goals ).length;
+	let incomplete = total - complete;
 
-		resolve( {
-			type: ARGS.archive ? 'archive' : 'active',
-			total,
-			complete,
-			incomplete,
-		} );
-	} );
+	return {
+		type: ARGS.archive ? 'archive' : 'active',
+		total,
+		complete,
+		incomplete,
+	};
 }
+
+export default makeCommand( progress, [ () => Promise.resolve() ] );
